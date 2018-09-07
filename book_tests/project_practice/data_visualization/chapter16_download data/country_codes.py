af5fb42b5580845f964766ef3f0048bcfefe4ded
iPine <931514355@qq.com>
2018-09-07 21:27:47 +0800

    chapter16 almost finished

--- book_tests/project_practice/data_visualization/chapter16_download data/country_codes.py
+++ book_tests/project_practice/data_visualization/chapter16_download data/country_codes.py
@@ -12,6 +12,30 @@ def get_country_code(country_name):
     for code, name in COUNTRIES.items():
         if name == country_name:
             return code
+        elif name == 'East Asia & Pacific (all income levels)':
+            return 'ai'
+        elif name == 'East Asia & Pacific (developing only)':
+            return 'ad'
+        elif name == 'Europe & Central Asia (all income levels)':
+            return 'el'
+        elif name == 'Europe & Central Asia (developing only)':
+            return 'ed'
+        elif name == 'Heavily indebted poor countries (HIPC)':
+            return 'hp'
+        elif name == 'Latin America & Caribbean (all income levels)':
+            return 'll'
+        elif name == 'Latin America & Caribbean (developing only)':
+            return 'ld'
+        elif name == 'Least developed countries: UN classification':
+            return 'lu'
+        elif name == 'Middle East & North Africa (all income levels)':
+            return 'ml'
+        elif name == 'Middle East & North Africa (developing only)':
+            return 'md'
+        elif name == 'Sub-Saharan Africa (all income levels)':
+            return 'sl'
+        elif name == 'Sub-Saharan Africa (developing only)':
+            return 'sd'
     #如果没有找到指定的国家，就返回None
     return None
 
