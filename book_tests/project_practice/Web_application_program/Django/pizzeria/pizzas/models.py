2e1b6796c6f5983787f87036be4fa1b5d3c45cee
ipine <931514355@qq.com>
2018-09-12 10:04:44 +0800

    web app-Chapter18.2

---
+++ book_tests/project_practice/Web_application_program/Django/pizzeria/pizzas/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+
+# Create your models here.
+
+class Pizza(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+
+
+class Topping(models.Model):
+    """docstring for Topping"""
+    name = models.TextField()
+    pizza = models.ForeignKey(Pizza,on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+        
