2aae86592c6fb65221cef7e27734e2844a8186b9
ipine <931514355@qq.com>
2018-09-16 21:38:05 +0800

    chapter19.1.2 finished

--- book_tests/project_practice/Web_application_program/Django/learning_log/learning_logs/views.py
+++ book_tests/project_practice/Web_application_program/Django/learning_log/learning_logs/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
+#新网页new_topic的视图函数需要的包
+from django.http import HttpResponseRedirect
+# from django.core.urlresolvers import reverse
+from django.urls import reverse
+
 from .models import Topic
+from .forms import TopicForm,EntryForm
+
+
+
 # Create your views here.
 def index(request):
     """学习笔记的主页"""
@@ -18,3 +27,41 @@ def topic(request,topic_id):
     entries = topic.entry_set.order_by('-date_added')
     context = {'topic':topic, 'entries':entries}
     return render(request,'learning_logs/topic.html',context)
+
+
+def new_topic(request):
+    """添加新主题"""
+    if request.method != 'POST':
+        #未提交数据，则创建一个新表单
+        form = TopicForm()
+    else:
+        #POST提交的数据，对数据进行处理
+        form = TopicForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('learning_logs:topics'))
+
+    context = {'form':form}
+    return render(request,'learning_logs/new_topic.html',context)
+
+
+def new_entry(request,topic_id):
+    """在特定的主题中添加新条目"""
+    topic = Topic.objects.get(id=topic_id)
+
+    if request.method != 'POST':
+        #未提交数据，则创建一个新表单
+        form = EntryForm()
+    else:
+        #POST提交的数据，对数据进行处理
+        form = EntryForm(data=request.POST)
+        if form.is_valid():
+            #不保存到数据库
+            new_entry = form.save(commit=False)
+            new_entry.topic = topic
+            new_entry.save()
+            #获取topic页面的URL，并传给HttpResponseRedirect，再重定向到特定的id为topic_id的topic页面
+            return HttpResponseRedirect(reverse('learning_logs:topic',args=[topic_id]))
+
+    context = {'topic':topic, 'form':form}
+    return render(request,'learning_logs/new_entry.html',context)
